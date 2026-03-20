8fee68f5a1900b01b2f1d8fae77ce9dfe4d9b15c
Jibao Mansaray <jibaomansaray@gmail.com>
2026-03-20 18:45:47 +1000

    just return the page

--- projects/backend/src/dirtybase_entry/http/api/v1/tip_strategy_controller.rs
+++ projects/backend/src/dirtybase_entry/http/api/v1/tip_strategy_controller.rs
@@ -8,6 +8,5 @@ use crate::dirtybase_entry::model::tip_strategy::TipStrategyRepo;
 pub async fn list_handler(
     CtxExt(mut tip_srategy_repo): CtxExt<TipStrategyRepo>,
 ) -> impl IntoResponse {
-    let result = ApiResponse::from(tip_srategy_repo.paginate(None).await);
-    result
+    ApiResponse::from(tip_srategy_repo.paginate(None).await)
 }
